82c161b0f6141c2ebf8a1ff70b6b4152849fb603
matteodisabatino <matteo.di.sabatino.1989@gmail.com>
2016-09-06 23:16:00 +0200

    Update apio_updater.sh

--- apio_updater.sh
+++ apio_updater.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 git config --global user.email "alessandrochelli@gmail.com"
 git config --global user.name "alechelli"
+cp -f Identifier.apio Identifier.old.apio
 git stash
 git pull
+cp -f Identifier.old.apio Identifier.apio
+rm Identifier.old.apio
 npm install
 bower install --allow-root
